ddaafb53971daae1b16c11aa127843c634d78df0
sonieva <s.onieva@sapalomera.cat>
2025-02-03 18:33:29 +0100

    fix(routes): update teacher route to use lazy loading for component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { TeacherComponent } from './pages/teacher/teacher.component';
 
 export const routes: Routes = [
-    { path: 'teacher', component: TeacherComponent },
-    {path: 'admin', loadComponent: () => import('./pages/admin/admin.component').then(mod => mod.AdminComponent) },
+  { path: 'teacher', loadComponent: () => import('./pages/teacher/teacher.component').then(mod => mod.TeacherComponent) },
+  { path: 'admin', loadComponent: () => import('./pages/admin/admin.component').then(mod => mod.AdminComponent) },
 ];
