78ca06213b0e614e36b730e667bdd12e3509c0de
Andreu <andreu.s.g30@gmail.com>
2025-02-03 20:20:26 +0100

    feat: teacher dashboard

--- frontend/src/app/components/teacher-dashboard/teacher-dashboard.component.ts
+++ frontend/src/app/components/teacher-dashboard/teacher-dashboard.component.ts
@@ -12,7 +12,7 @@ export class TeacherDashboardComponent {
     { title: "Control d’assistència", description: "Registra l’assistència de les teves classes.", image: "assets/attendance.jpg", buttonText: "PASSAR LLISTA" },
     { title: "Projectes", description: "Administra els projectes dels teus grups.", image: "assets/projects.jpg", buttonText: "GESTIONAR PROJECTES" },
     { title: "Rúbriques", description: "Gestiona les rúbriques pels teus grups.", image: "assets/rubrics.jpg", buttonText: "GESTIONAR RÚBRIQUES" },
-    { title: "Bústia de propostes", description: "Revisa les propostes dels teus alumnes.", image: "assets/proposals.jpg", buttonText: "VEURE PROPOSTES" },
+    { title: "Bústia de propostes", description: "Revisa les propostes dels teus alumnes.", image: "assets/proposal.jpg", buttonText: "VEURE PROPOSTES" },
     { title: "Calendari", description: "Estableix les dates d’assignatures i projectes.", image: "assets/calendar.jpg", buttonText: "GESTIONAR CALENDARI" },
     { title: "Informes", description: "Consulta els informes d’assistència i de progrés.", image: "assets/reports.jpg", buttonText: "VEURE INFORMES" }
   ];
