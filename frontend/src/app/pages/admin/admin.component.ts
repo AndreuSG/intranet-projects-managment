8f97c470e7c656b2745b16d966706355c46faa35
sonieva <s.onieva@sapalomera.cat>
2025-02-05 15:35:47 +0100

    feat(admin): add shared button component and enhance admin page layout

--- frontend/src/app/pages/admin/admin.component.ts
+++ frontend/src/app/pages/admin/admin.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { BackButtonComponent } from "../../shared/back-button/back-button.component";
+import { ButtonComponent } from "../../shared/button/button.component";
 
 @Component({
   standalone: true,
   selector: 'app-admin',
   imports: [
     MatIconModule,
-    BackButtonComponent
+    BackButtonComponent,
+    ButtonComponent
 ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
